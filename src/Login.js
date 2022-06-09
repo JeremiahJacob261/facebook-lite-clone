b5de8d5f176aaf2c1027fc08cff1fbc2b4f3aa21
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-09 05:30:06 +0100

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -2,33 +2,43 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
-  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  function getValueU(username) {
-    setUsername(username.target.value);
+  function getValueU(email) {
+    setEmail(email.target.value);
   }
   function getValueP(password) {
     setPassword(password.target.value);
   }
   function login() {
-    if (username == password) {
-      alert('yes');
-    } else {
-      alert('Invalid password or email');
-    }
+  
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+    
   }
   return (
     <div>
           <div>
             <h2>LOGIN</h2>
             <input
-              type="text"
-              placeholder="username"
+              type="email"
+              placeholder="email"
               id="textfield"
               onChange={getValueU}
-              value={username}
+              value={email}
             />
             <br />
             <input
